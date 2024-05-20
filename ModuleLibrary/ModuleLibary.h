5556a472d642673c446321e4258c121ebc96f5fa
Proddy <proddy@users.noreply.github.com>
2024-05-20 21:08:18 +0200

    Update ModuleLibary.h

--- ModuleLibrary/ModuleLibary.h
+++ ModuleLibrary/ModuleLibary.h
@@ -1,1 +1,20 @@
+#ifndef MODULELIBRARY_H
+#define MODULELIBRARY_H
 
+#include <Arduino.h>
+#include <memory>
+#include <vector>
+
+class Module {}; // forward declaration
+
+class ModuleLibrary {
+  public:
+    void setup();
+    void loop();
+    void status();
+
+  private:
+    std::vector<std::unique_ptr<Module>> modules;
+};
+
+#endif
