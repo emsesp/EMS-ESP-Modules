c1762ffa074d5757b2855357994cf043b0768715
Proddy <proddy@users.noreply.github.com>
2024-05-24 18:40:05 +0200

    Update extra_script.py

--- extra_script.py
+++ extra_script.py
@@ -1,7 +1,12 @@
 Import('env')
 from os.path import join, realpath
 
+if env.get('BOARD_MCU'):
+    board = env.get('BOARD_MCU')
+else:
+    board = env.get('PIOPLATFORM')
+
 env.Append(
-    LIBPATH=[realpath(join(env.get('BOARD_MCU')))],
+    LIBPATH=[realpath(board)],
     LIBS=['Module','ModuleTest1','ModuleTest2']
 )
