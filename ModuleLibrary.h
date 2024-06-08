644d83b67ef1583bf4fa5d1efe571de4d7ae2a38
Proddy <proddy@users.noreply.github.com>
2024-06-08 12:23:58 +0200

    Add files via upload

--- ModuleLibrary.h
+++ ModuleLibrary.h
@@ -12,22 +12,22 @@ class ModuleLibrary {
   public:
     class Modules {
       public:
-        Modules(const char * name, std::unique_ptr<Module> module)
-            : name(name)
+        Modules(const char * key, std::unique_ptr<Module> module)
+            : key(key)
             , module(std::move(module)) {
         }
-        const char *            name;
+        const char *            key; // internal key for the module
         std::unique_ptr<Module> module;
     };
 
-    void start(emsesp::EMSESP *);
+    void start(emsesp::EMSESP * emsesp_main, bool test_mode = false);
     void loop();
     void list(JsonObject output);
-
-    void enable(const char * name, bool enable);
+    bool enable(const char * key, const char * license, bool enable);
 
   private:
     std::vector<Modules>     modules_;
+    std::string              mac_address_;
     static uuid::log::Logger logger_;
 };
 
