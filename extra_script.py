cce72904b5b05a775332676c067d8b76c989d984
Proddy <proddy@users.noreply.github.com>
2024-07-23 10:22:45 +0200

    Add files via upload

--- extra_script.py
+++ extra_script.py
@@ -1,13 +1,12 @@
-# This file is auto generated
-# Do not modify this file
+# This file is auto generated. Do not modify.
 
 Import('env')
 from os.path import realpath
 
-platform = env.get('PIOPLATFORM')
 board = env.get('BOARD_MCU')
 
-env.Append(
-  LIBPATH=[realpath(board)],
-  LIBS=['Module']
-)
+if board:
+  env.Append(
+    LIBPATH=[realpath(board)],
+    LIBS=['Module']
+  )
